7de355522ae5a762dddac05c853addf65bdb9444
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-28 12:39:27 +0200

    bug fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creating a user table
 -- with a unique email address
 CREATE TABLE IF NOT EXISTS users(
-	id INT AUTO_INCRMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
 	);
