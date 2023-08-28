fbc439990922fa8cacfbcfa1d5b0433bc9332d08
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-29 01:02:40 +0200

    functions

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- a function in mysql
+DELIMITER $$ ;
+CREATE FUNCTION IF NOT EXISTS SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	IF (b = 0) THEN
+		RETURN (0);
+	ELSE
+		RETURN (a / b);
+	END IF;
+END;$$
