cb90e2a20c6023e8248c8cccb3d3ad55d4b4fbc6
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-28 13:21:33 +0200

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- creating a user table
+-- with a unique email address
+CREATE TABLE IF NOT EXISTS users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	);
