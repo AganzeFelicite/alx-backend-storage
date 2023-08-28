4d3438aaaf011d8708b651429b09fd884e58313b
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-28 17:55:06 +0200

    . Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- Email validation to sent not sent
+DELIMITER $$ ;
+CREATE TRIGGER resets_valid_email AFTER UPDATE ON users 
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		UPDATE users set valid_email = 0;
+	END IF;
+END;$$
