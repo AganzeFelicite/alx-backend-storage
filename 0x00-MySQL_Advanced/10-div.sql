b6f20fd50c5f9813fe52d2afadf9b424e88a5015
AganzeFelicite <aganzefelicite@gmail.com>
2023-08-29 10:49:36 +0200

    division

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,18 +1,15 @@
 -- script to devide in a function
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-
-CREATE FUNCTION IF NOT EXISTS SafeDiv(
-    a INT,
-    b INT
-)
+CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
+    IF (b = 0)
+    THEN
+        RETURN (0);
     ELSE
-        RETURN a / b;
+        RETURN (a / b);
     END IF;
-END $$
-
+END
+$$ 
 DELIMITER ;
-
