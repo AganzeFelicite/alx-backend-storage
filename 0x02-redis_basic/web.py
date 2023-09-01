e1e552698a885e42c2e4b7cb81fe3ac2e609e385
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-01 17:11:24 +0200

    advanced task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+this get
+5. Implementing an expiring web cache and track
+"""
+
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+r = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a requesthas been made """
+
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
+def get_page(url: str) -> str:
+    """Uses the requests module to obtain the HTML
+    content of a particular URL and returns it.
+    """
+    req = requests.get(url)
+    return req.text
