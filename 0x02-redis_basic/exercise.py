427f6fe82bbe8422fc21153111988d1e91cd8e6c
AganzeFelicite <aganzefelicite@gmail.com>
2023-09-01 12:20:27 +0200

    cconverting bytes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,11 +5,12 @@ to redis
 """
 
 from uuid import uuid4
+import sys
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
-UnionOfTypes = Union[str, bytes, int, float]
+Types = Union[str, bytes, int, float]
 
 
 class Cache:
@@ -20,7 +21,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: UnionOfTypes) -> str:
+    def store(self, data: Types) -> str:
         """
         this is a method to store into
         the database
@@ -28,3 +29,28 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Types:
+        """
+        this method returns stored
+        values from redis and convert
+        the b to utf-8
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_str(self: bytes) -> str:
+        """
+        this method convert a byte back
+        to utf-8
+        """
+        return self.decode("utf-8")
+
+    def get_int(self: bytes) -> str:
+        """
+        this converts a byte back to
+        integer
+        """
+        return self.from_bytes(self, sys.byteorde)  
